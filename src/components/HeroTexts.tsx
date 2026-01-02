1d1107686dafd63f75a7e4e24cd0fc06c0e7b052
koushik-0410 <koushik.sethuraman06@gmail.com>
2026-01-02 17:33:26 +0530

    names

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -2,20 +2,23 @@ import TextRotator from "./TextRotator";
 import { portfolioConfig } from "@/config/portfolio.config";
 
 const HeroTexts = () => {
-  // Get the name parts
   const nameParts = portfolioConfig.name.split(" ");
   const firstName = nameParts[0];
-  const middleName = nameParts.length > 2 ? nameParts[1] : "";
-  const lastName = nameParts.length > 2 ? nameParts[2] : nameParts[1];
+  const lastName = nameParts[1];
 
   return (
     <>
       <h3 className="font-poppins text-2xl max-sm:text-xl">My Name is</h3>
-      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl ">
-        {firstName} {middleName} <br /> {lastName} .
+
+      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl mt-6">
+        {firstName}
+        <br />
+        <span className="text-7xl max-sm:text-5xl">.{lastName}</span>
       </h1>
+
       <TextRotator />
     </>
   );
 };
+
 export default HeroTexts;
