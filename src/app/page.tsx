77b69b17b44093a71ef3b202915e727bc23236c4
koushik-0410 <koushik.sethuraman06@gmail.com>
2026-01-04 19:41:59 +0530

    page.tsx edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,31 @@ import FramerWrapper from "@/components/animation/FramerWrapper";
 import Aboutfooter from "@/components/Aboutfooter";
 import Heading from "@/components/Heading";
 import { Badge } from "@/components/ui/badge";
-import { Circle, Heart, User2 } from "lucide-react";
+import {
+  Circle,
+  Heart,
+  User2,
+  LightbulbIcon,
+  Briefcase,
+  Layers,
+  Phone,
+  PackagePlus,
+  ExternalLink,
+} from "lucide-react";
 import { portfolioConfig } from "@/config/portfolio.config";
+import SkillsFooter from "@/components/SkillsFotter";
+import ProjectCards from "@/components/ProjectsCard";
+import ContactForm from "@/components/ContactForm";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   const items = portfolioConfig.about.hobbies.map((hobby) => ({ hobby }));
@@ -89,6 +112,184 @@ export default function Home() {
             })}
           </div>
         </FramerWrapper>
+      </div>
+
+      {/* SKILLS SECTION */}
+      <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+        <Badge variant="secondary" className="gap-1.5 py-1 ">
+          <LightbulbIcon className="w-4 h-4" />
+          My Skills
+        </Badge>
+        <div className="flex flex-col gap-3">
+          <Heading>Technical Skills & Experience</Heading>
+          <FramerWrapper y={0} x={200}>
+            <p className="font-poppins text-xl w-full text-primary max-sm:text-lg">
+              I am a Computer Science student with a good understanding of
+              programming languages such as Java, Python, and C++. I enjoy
+              learning how software and applications work by writing code and
+              building small projects. Along with programming, I have basic
+              knowledge of web technologies like HTML, CSS, and JavaScript. I
+              continuously improve my skills by practicing regularly, exploring
+              new concepts, and applying what I learn in real projects..
+            </p>
+          </FramerWrapper>
+          <FramerWrapper y={100} delay={0.3} className="block w-full">
+            <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl mb-4">
+              Programming Languages
+            </h1>
+            <div className="w-full grid grid-cols-7 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4">
+              <SkillsFooter
+                items={portfolioConfig.skills.programmingLanguages}
+              />
+            </div>
+          </FramerWrapper>
+          <FramerWrapper className="block w-full" y={100} delay={0.32}>
+            <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl mb-4">
+              Framework/Libraries
+            </h1>
+            <div className="w-full grid grid-cols-7 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4">
+              <SkillsFooter items={portfolioConfig.skills.frameworks} />
+            </div>
+          </FramerWrapper>
+          <FramerWrapper className="block w-full" y={100} delay={0.34}>
+            <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl mb-4">
+              Tools & Technologies
+            </h1>
+            <div className="w-full grid grid-cols-7 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4">
+              <SkillsFooter items={portfolioConfig.skills.tools} />
+            </div>
+          </FramerWrapper>
+        </div>
+      </div>
+
+      {/* EDUCATION SECTION */}
+      <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+        <Badge variant="secondary" className="gap-1.5 py-1 ">
+          <Briefcase className="h-4 w-4" />
+          Education
+        </Badge>
+        <div className="flex flex-col gap-3">
+          <Heading>My Education</Heading>
+        </div>
+        <div className="w-full h-fit flex flex-col">
+          {portfolioConfig.education.map((edu, index) => (
+            <div className="w-full h-fit flex" key={index}>
+              <FramerWrapper
+                y={0}
+                x={-100}
+                delay={0.35 + index * 0.1}
+                className="w-1/4 font-rubik flex items-center justify-evenly text-lg max-sm:text-base"
+              >
+                {edu.period}
+              </FramerWrapper>
+              <FramerWrapper
+                y={0}
+                x={100}
+                delay={0.35 + index * 0.1}
+                className="relative w-3/4 border-l-4 border-l-[#3c3c3c] p-4 gap-3 education_point"
+              >
+                <div className="text-2xl font-rubik max-sm:text-xl">
+                  {edu.degree}, <br /> {edu.institution}
+                </div>
+                <p className="font-poppins text-base w-full text-primary max-sm:text-xs">
+                  {edu.description}
+                </p>
+              </FramerWrapper>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* PROJECTS SECTION */}
+      <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+        <Badge variant="secondary" className="gap-1.5 py-1 ">
+          <Layers className="h-4 w-4" />
+          Projects
+        </Badge>
+        <div className="flex flex-col gap-3">
+          <Heading>My Projects</Heading>
+          <FramerWrapper y={0} x={200}>
+            <p className=" font-poppins text-lg w-full text-primary max-sm:text-base">
+              I build projects to practice my skills and explore new web
+              technologies. Each project reflects my interest in problem
+              solving, learning new things, and creating applications that are
+              simple and easy to use
+            </p>
+          </FramerWrapper>
+        </div>
+
+        <div className=" w-full flex flex-row flex-wrap gap-3 max-lg:flex-col">
+          {portfolioConfig.projects.map((val, indx) => {
+            return <ProjectCards key={indx} value={val} num={indx} />;
+          })}
+        </div>
+      </div>
+
+      {/* CONTACT SECTION */}
+      <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+        <Badge variant="secondary" className="gap-1.5 py-1 ">
+          <Phone className="h-4 w-4" />
+          Contact Us
+        </Badge>
+        <div className="flex flex-col gap-3 w-full">
+          <Heading>Contact Me!</Heading>
+          <div className="h-auto w-full flex justify-center items-center">
+            <FramerWrapper y={0} scale={0.8}>
+              <ContactForm />
+            </FramerWrapper>
+          </div>
+          <p className=" font-poppins text-lg w-full text-primary max-sm:text-base"></p>
+        </div>
+      </div>
+
+      {/* MORE SECTION */}
+      <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+        <Badge variant="secondary" className="gap-1.5 py-1 ">
+          <PackagePlus className="h-4 w-4" />
+          More
+        </Badge>
+        <div className="flex flex-col gap-3">
+          <Heading>More</Heading>
+        </div>
+        <div className="h-auto w-full flex flex-wrap gap-3 p-2">
+          {portfolioConfig.moreLinks.map((value, indx) => {
+            return (
+              <FramerWrapper
+                key={indx}
+                className="max-w-[32%] max-lg:max-w-full"
+                y={0}
+                scale={0.8}
+                delay={indx / 4}
+                duration={0.15}
+              >
+                <Card className="w-full">
+                  <CardHeader>
+                    <CardTitle>{value.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-base font-poppins ">
+                      {value.description}
+                    </p>
+                  </CardContent>
+                  <CardFooter>
+                    <Link
+                      href={value.link}
+                      target="blank"
+                      className={cn(
+                        buttonVariants({ variant: "default", size: "lg" }),
+                        "w-full gap-3"
+                      )}
+                    >
+                      {" "}
+                      <ExternalLink />
+                      Visit here
+                    </Link>
+                  </CardFooter>
+                </Card>
+              </FramerWrapper>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
