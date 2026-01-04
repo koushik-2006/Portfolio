f60bec9564dd86879569db81426a2016030285a1
koushik-0410 <koushik.sethuraman06@gmail.com>
2026-01-04 21:04:52 +0530

    resume change 2

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -4,7 +4,12 @@ import HackerBtn from "./animation/HackerBtn";
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      <a href="/Koushik_Resume.pdf" download>
+      <a
+        href="/Portfolio/Koushik_Resume.pdf"
+        download
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <HackerBtn label="Download Resume" />
       </a>
     </div>
