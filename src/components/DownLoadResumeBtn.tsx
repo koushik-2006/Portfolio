5577e6dc6b4ae436794dec21bac5f7afeb729679
koushik-0410 <koushik.sethuraman06@gmail.com>
2026-01-04 20:58:46 +0530

    resume changes

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -1,17 +1,14 @@
-import { cn } from '@/lib/utils'
-import { Download } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-import { buttonVariants } from './ui/button'
-import HackerBtn from './animation/HackerBtn'
+import React from "react";
+import HackerBtn from "./animation/HackerBtn";
 
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-  
-    <HackerBtn label='Download Resume' />
-  </div>
-  )
+      <a href="/Koushik_Resume.pdf" download>
+        <HackerBtn label="Download Resume" />
+      </a>
+    </div>
+  );
 }
 
-export default DownLoadResumeBtn
+export default DownLoadResumeBtn;
